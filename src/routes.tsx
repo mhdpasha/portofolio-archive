3b780b9f0bf5997604496af3b52e2e2425a46e44
Pasha <pashaalbara01@gmail.com>
2024-05-03 22:52:31 +0700

    fix: index.ts

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,5 @@
 import Transition from './transition'
-import Home from './pages/Home'
-import About from './pages/About'
-import Projects from './pages/Projects'
+import { Home, About, Projects } from './pages'
 
 export const routes = [
     { name: 'Home', path: '/', component: Transition(Home) },
